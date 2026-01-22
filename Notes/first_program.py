5f0f26875f4bb0add9edc9c1437620b2e54ef086
Imtiredguys <gracie.patterson@ucas-edu.net>
2026-01-22 11:42:38 -0700

    Finished Variables Practice

---
+++ Notes/first_program.py
@@ -0,0 +1,5 @@
+#GP, First Program Python
+
+name = input("What is your name: ")
+
+print("Hello", name, "it's nice to meet you!")
---
+++ Notes/variable_practices.py
@@ -0,0 +1,19 @@
+# GP, Variables Practice
+
+name = "Patterson"
+
+number = 11
+
+number2 = 803
+
+school = "UCAS"
+
+year = 2026
+
+eye_color = "Hazel"
+
+age = 14
+
+favorite_subject_in_school = "Favorite school subject changes depending on the day"
+
+print(name, number, number2, school, year, eye_color, age, favorite_subject_in_school)
