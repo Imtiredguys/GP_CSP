f5e17a6b545a12f0921023596117ff7a0435b17c
Imtiredguys <gracie.patterson@ucas-edu.net>
2026-01-27 11:50:20 -0700

    Strings

--- Notes/integers_floats_pt2.py
+++ Notes/integers_floats_pt2.py
@@ -1,8 +1,8 @@
 #GP, Floats and Integers Part 2; Finanical Calculator 
 
-monthly_income =print("What is your monthly income? $")
+monthly_income =float(imput("What is your monthly income? $"))
 
-rent = print("What is the cost your monthly rent/mortgage? $")
+rent = float(imput("What is the cost your monthly rent/mortgage? $")
 
 utilities = print("What is the cost of your monthly utilities? $")
 
@@ -10,15 +10,17 @@ groceries = print("What is the cost of monthly groceries? $")
 
 transportation = print("What is the cost of monthly transportantion? $")
 
-save_percentage = print("How much of your income should you save? $")
+savings = print("How much of your income should you save? $")
 
-rent_percentage = ((rent/monthly_income)*100)
+rent_percentage = (rent/monthly_income)*100
 
 utilities_percentage = ((utilities/monthly_income)*100)
 
 groceries_percentage = ((groceries/monthly_income)*100)
 
 transportation_percentage = ((transportation/monthly_income)*100)
+
+save_percentage = ((savings/monthly_income)*100)
 
 rent_cost = (rent + rent_percentage)
 
@@ -28,6 +30,6 @@ groceries_cost = (groceries + groceries_percentage)
 
 transportation_cost = (transportation + transportation_percentage)
 
-money_saved = (save_percentage*100)
+money_saved = (savings + save_percentage)
 
-monthly_spending = (save_percentage + rent_cost + utilities_cost + groceries_cost + transportation_cost)
+monthly_spending = (money_saved + rent_cost + utilities_cost + groceries_cost + transportation_cost)
---
+++ Notes/strings.py
@@ -0,0 +1,24 @@
+#GP, Strings
+
+""""color = input("What is your favorite color? ").strip().upper()
+
+print(color, "is a really cool color!")"""
+
+'''alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+sentence = "The quick brown fox jumps over the lazy dog"
+
+"""print(alphabet[4:8])"""
+word = input("What word do you want to change? ").strip()
+new = input('What new word will be?' )
+start = sentence.find(word)
+print('('+sentence[start:start+len(word)]+')')
+
+print(sentence)
+
+sentence = sentence.replace(word, new)
+
+print(sentence)'''
+
+number = int(input("Give me a base 10 number: "))
+print(number/5)
