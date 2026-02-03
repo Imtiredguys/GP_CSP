859fc8b5fb7b5e8a7b735f1cab76ede74edc0500
Imtiredguys <gracie.patterson@ucas-edu.net>
2026-02-03 15:48:20 -0700

    iamstillsupertired

--- Notes/fuzzbuzz.py
+++ Notes/fuzzbuzz.py
@@ -3,11 +3,12 @@
 start = 0
 
 while start <= 50:
-    print(start)
     start += 1
-    if start%3 == 0:
-        print("Fizz")
-    if start%5 == 0:
-        print("Buzz")
     if start%5 == 0 and start%3 == 0:
         print("FizzBuzz")
+    elif start%3 == 0:
+        print("Fizz")
+    elif start%5 == 0:
+        print("Buzz")
+    else:
+        print(start)
