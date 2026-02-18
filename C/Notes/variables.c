02a3af0be1e2e859a6cb081b0e2aa81c4850e261
Imtiredguys <gracie.patterson@ucas-edu.net>
2026-02-18 11:49:36 -0700

    firstdaydoingc

---
+++ C/Notes/variables.c
@@ -0,0 +1,11 @@
+// GP, Variable Notes For C
+#include <stdio.h> // lets us create inputs and outputs
+
+int main() { // always has to be inside of a function
+    int number = 12; // every statement has to end in a semi collen // must put int, float, or char before variable name
+    float pi = 3.14;
+    printf("We have the number %d\n", number); // d is a placeholder for int // \n tells the computer to move to the next line
+    printf("%f\n", pi); // f is a place holder for float
+    return 0; //always is the last line of main
+}
+
