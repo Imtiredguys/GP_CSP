c3f0b3aca5de39fae9b052fdceaf2b74e52ea388
Imtiredguys <gracie.patterson@ucas-edu.net>
2026-02-19 11:49:20 -0700

    idkifyallknowthisbutimtirdguys

--- C/Notes/variables.c
+++ C/Notes/variables.c
@@ -4,8 +4,16 @@
 int main() { // always has to be inside of a function
     int number = 12; // every statement has to end in a semi collen // must put int, float, or char before variable name
     float pi = 3.14;
+    char name[] = "Xavier";
+    char person[50];
+    int age; // starting point for setting up an input // 1) data type name;
+    printf("Tell me how old you are\n"); // 2) print instructions for user
+    scanf("%d", &age); // last part of setting up an input // 3) taken in input using: scanf("%datatype", &variable_name);
+    printf("Tell me your name\n");
+    scanf("%s", &person);
     printf("We have the number %d\n", number); // d is a placeholder for int // \n tells the computer to move to the next line
     printf("%f\n", pi); // f is a place holder for float
+    printf("%s is %d years old\n", person, age); // s is a placeholder for char (strings) // also printf means print format you always use printf when using C
     return 0; //always is the last line of main
 }
 
---
+++ C/Practices/variables_practice.c
@@ -0,0 +1,17 @@
+// GP, Variables Practice for C
+#include <stdio.h>
+
+int main() {
+    char name[] = "Patterson";
+    char eye_color[] = "Hazel?";
+    int age = 14;
+    char favorite_color = "Blue";
+    int unlucky_number = 11;
+    char breakfast = "Bagel";
+    char school = "UCAS";
+    float GDP = 3.5;
+    char favorite_subject = "It depends on the day and how tired I am";
+    int year = 2026;
+    printf("This student's name is %s", name);
+    return 0;
+}
