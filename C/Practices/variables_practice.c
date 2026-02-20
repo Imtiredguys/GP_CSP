a7c69cd81d24f26789ff8cd257d3fbec084b5e2a
Imtiredguys <gracie.patterson@ucas-edu.net>
2026-02-20 15:53:01 -0700

    ithinkigotitbutiguesswellseelater

--- C/Practices/variables_practice.c
+++ C/Practices/variables_practice.c
@@ -5,13 +5,13 @@ int main() {
     char name[] = "Patterson";
     char eye_color[] = "Hazel?";
     int age = 14;
-    char favorite_color = "Blue";
+    char favorite_color[] = "Blue";
     int unlucky_number = 11;
-    char breakfast = "Bagel";
-    char school = "UCAS";
-    float GDP = 3.5;
-    char favorite_subject = "It depends on the day and how tired I am";
+    char breakfast[] = "Bagels";
+    char school[] = "UCAS";
+    float GPA = 3.5;
+    char favorite_subject[] = "It depends on the day and how tired I am";
     int year = 2026;
-    printf("This student's name is %s", name);
+    printf("Student mini profile:\n \nStudent Name: %s\nEye color: %s\nAge: %d\nFovorite color: %s\n'Unkucky Number': %d\nFavorite breakfast food: %s\nSchool: %s (obviously)\nGPA: %f (estimated)\nFavorit subject: %s\nCurrent year: %d", name, eye_color, age, favorite_color, unlucky_number, breakfast, school, GPA, favorite_subject, year);
     return 0;
 }
