e273bd852b610798f37c281d4742b272dc011f61
Imtiredguys <gracie.patterson@ucas-edu.net>
2026-04-27 11:49:09 -0600

    parkcityyippe

--- webdev/Notes/FLASK/app.py
+++ webdev/Notes/FLASK/app.py
@@ -4,22 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hello World</p>"
+    return render_template("name.html", title="Home Page", content="This is the homepage of my website")
 
 @app.route("/<name>")
 def user(name):
-    return f"Hello {name}!"
+    return render_template("name.html", title=f"{name}'s Page", content=f"This is the individualized content for {name}")
 
 @app.route('/test')
 def testing():
     return "<p>This is a test</p>"
 
-'''@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         name = request.form['username']
         return f"Hello {name}!"
-    return render_template('webdev/Notes/FLASK/name.html')'''
+    return render_template('webdev/Notes/FLASK/name.html')
     
 
 if __name__ == '__main__':
